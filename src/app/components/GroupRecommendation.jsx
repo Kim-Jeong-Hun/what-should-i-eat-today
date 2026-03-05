e1f6a9758e02cd76057e50d28cf15d94d9917f65
Kim Jeong Hun <rlawjdgns135@naver.com>
2026-03-06 00:16:32 +0900

    혼밥 컴포넌트, 단체 컴포넌트 생성

---
+++ src/app/components/GroupRecommendation.jsx
@@ -0,0 +1,7 @@
+export default function SoloRecommendation () {
+    return (
+        <div>
+            단체 메뉴 추천
+        </div>
+    )
+}
---
+++ src/app/components/SoloRecommendation.jsx
@@ -0,0 +1,7 @@
+export default function SoloRecommendation () {
+    return (
+        <div>
+            개인 메뉴 추천
+        </div>
+    )
+}
